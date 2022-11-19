ff90948e83ec7656e0fde88fba57bb43612e003f
Luzmy97 <118416340+Luzmy97@users.noreply.github.com>
2022-11-19 11:25:52 -0600

    Alfabeto-Mixteco
    Cambio de alfabeto al mixteco

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -2,7 +2,7 @@ export const CONFIG = {
   tries: 6, // This changes how many tries you get to finish the wordle
   language: 'Ayuujk', // This changes the display name for your language
   wordLength: 5, // This sets how long each word is based on how many characters (as defined in orthography.ts) are in each word
-  author: 'lbalbuenag', // Put your name here so people know who made this Wordle!
+  author: 'Mary', // Put your name here so people know who made this Wordle!
   authorWebsite: 'http://ayuujkjaaky.tlahuitoltepec.com/', // Put a link to your website or social media here
   wordListSource: 'Appyuujk', // Describe the source material for your words here
   wordListSourceLink: 'http://ayuujkjaaky.tlahuitoltepec.com/ayuujk_assets/js/wordle.js', // Put a link to the source material for your words here
@@ -13,7 +13,7 @@ export const CONFIG = {
   shuffle: true, // whether to shuffle the words in the wordlist each time you load the app (note: you will lose the 'word of the day' functionality if this is true)
   normalization: 'NFC', // whether to apply Unicode normalization to words and orthography - options: 'NFC', 'NFD', 'NKFC', 'NKFD', false
   startDate: 'January 1, 2022 00:00:00', // what date and time to start your game from
-  defaultLang: 'ay', // the default interface language
-  availableLangs: ['ay', 'es','en', 'sw', 'zh'], // the options available to the user for translation languages
+  defaultLang: 'mi', // the default interface language
+  availableLangs: ['ay', 'es','en', 'sw', 'zh','mix'], // the options available to the user for translation languages
   escapeSpecialCharacters: true, // whether to escape all characters in the orthography.
 }
